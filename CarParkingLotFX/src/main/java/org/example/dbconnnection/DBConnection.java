4b1d98fe2a8313b8a1f181a831e8bf632f9ac19e
sdjuraev <s.djuraev@newuu.uz>
2024-10-13 12:31:01 +0500

    V1

--- CarParkingLotFX/src/main/java/org/example/dbconnnection/DBConnection.java
+++ CarParkingLotFX/src/main/java/org/example/dbconnnection/DBConnection.java
@@ -13,51 +13,40 @@ public class DBConnection {
             e.printStackTrace();
         }
     }
-    private static void createTable(Connection conn) throws SQLException {
-        String sql = "CREATE TABLE IF NOT EXISTS students ("
-                + "id INTEGER PRIMARY KEY, "
-                + "name TEXT NOT NULL, "
-                + "age INTEGER);";
+    private static void createTable(Connection conn, String sqlCommand) throws SQLException {
         try (Statement stmt = conn.createStatement()) {
-            stmt.execute(sql);
+            stmt.execute(sqlCommand);
             System.out.println("Table created.");
         } }
-    private static void insertData(Connection conn, int id, String name, int
-            age)
-            throws SQLException {
-        String sql = "INSERT INTO students(id, name, age) VALUES(?, ?, ?);";
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
-            pstmt.setString(2, name);
-            pstmt.setInt(3, age);
+    private static void insertData(Connection conn, String sqlCommand) throws SQLException
+    {
+        try (PreparedStatement pstmt = conn.prepareStatement(sqlCommand))
+        {
             pstmt.executeUpdate();
-        } }
-    private static void readData(Connection conn) throws SQLException {
-        String sql = "SELECT id, name, age FROM students";
-        try (PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
-            System.out.println("ID | Name | Age");
-            System.out.println("----------------");
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-                System.out.printf("%d | %-5s | %d%n", id, name, age);
-            } }
+        }
     }
-    private static void updateData(Connection conn) throws SQLException {
-        String sql = "UPDATE students SET age = ? WHERE id = ?";
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, 23); // New age for Alice
-            pstmt.setInt(2, 1); // Update record with id = 1
+    private static void readData(Connection conn, String sqlCommand) throws SQLException {
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sqlCommand);
+             ResultSet rs = pstmt.executeQuery()) {
+            ResultSetMetaData metaData = rs.getMetaData();
+            int col = metaData.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= col; i++) {
+                    System.out.print(rs.getString(i)+" ");
+                }
+            }
+        }
+    }
+    private static void updateData(Connection conn, String sqlCommand) throws SQLException {
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sqlCommand)) {
             pstmt.executeUpdate();
-            System.out.println("Updated age");
+            System.out.println("Updated!");
         } }
-    private static void deleteData(Connection conn) throws SQLException {
-        String sql = "DELETE FROM students WHERE id = ?";
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, 2); // Delete record where id = 2
+    private static void deleteData(Connection conn, String sqlCommand) throws SQLException {
+        try (PreparedStatement pstmt = conn.prepareStatement(sqlCommand)) {
             pstmt.executeUpdate();
-            System.out.println("Deleted student with id = 2.");
+            System.out.println("Data deleted successfully !");
         } }
 }
